a426907d4651a7c43ff15fca2f1ed742692b836c
Joy Sen <develop.senjoy@gmail.com>
2024-08-15 19:10:19 +0600

    Add files via upload

---
+++ JavaScript/js/script.js
@@ -0,0 +1,40 @@
+ /* ex1 */
+function showText(){
+    document.getElementById('s_text').innerHTML="Hello World";
+}
+
+/* ex2 */
+function showDate(){
+    document.getElementById('s_date').innerHTML=Date();
+}
+
+/* ex3 */
+function bulbOn(){
+    document.getElementById('img').src="images/on.gif"
+}
+function bulbOff(){
+    document.getElementById('img').src="images/off.gif"
+}
+/* ex4 */
+function bulbOnO1(){
+    document.getElementById('imge').src='images/on.gif';
+}
+function bulbOff2(){
+    document.getElementById('imge').src='images/off.gif';
+}
+/* ex6 */
+function textColor(){
+	document.getElementById('color').style.color="blue";
+}
+/* ex7 */
+function textSize(){
+	document.getElementById('50px').style.fontSize="50px";
+}
+/* ex8 */
+function textHide(){
+	document.getElementById('text').style.display='none';
+}
+function textshow(){
+	document.getElementById('text').style.display='block';
+}
+/* ex9 */
